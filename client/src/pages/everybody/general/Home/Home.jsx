88b04a7803efa55f9c3b07a616360b4bb9d216e9
rodyherrera <contact@rodyherrera.com>
2024-01-03 22:03:13 -0300

    Update on the platform home page.

--- client/src/pages/everybody/general/Home/Home.jsx
+++ client/src/pages/everybody/general/Home/Home.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import Button from '@components/general/Button';
+import DashedContainer from '@components/general/DashedContainer';
 import { useSelector } from 'react-redux';
-import { authenticate } from '@services/github/actions';
+import './Home.css';
 
 const HomePage = () => {
-    const { isAuthenticated, user } = useSelector(state => state.auth);
+    const { isAuthenticated } = useSelector(state => state.auth);
 
     return (
         <main id='Home-Page-Main'>
+            <section id='Home-Left-Container'>
+                <article id='Home-Left-Container-Title-Container'>
+                    <h1 id='Home-Left-Container-Title'>Unlock your <br/>entrepreneurial spirit!</h1>
+                    <p id='Home-Left-Container-Subtitle'>
+                        <span>Invest in applications, not operations.</span>
+                        <DashedContainer>We handle the hard things.</DashedContainer>
+                    </p>
+                </article>
+                <article id='Home-Left-Container-Actions-Container'>
+                    {(isAuthenticated) ? (
+                        <Button title='Go to Dashboard' to='/dashboard/' />
+                    ) : (
+                        <Button title='Start Deploying' to='/auth/sign-in/' />
+                    )}
+                    <Button title='Learn More' variant='Contained' />
+                </article>
+            </section>
         </main>
     );
 };
