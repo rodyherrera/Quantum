13ffc13efe1d2a16eaad68787baa5d1ffd5121be
rodyherrera <contact@rodyherrera.com>
2024-01-08 15:16:52 -0300

    Execution of commands within the repository through the terminal (client).

--- client/src/pages/protected/repository/Terminal/Terminal.jsx
+++ client/src/pages/protected/repository/Terminal/Terminal.jsx
@@ -1,25 +1,85 @@
 import React, { useState, useEffect } from 'react';
+import Terminal, { ColorMode, TerminalOutput } from 'react-terminal-ui';
+import { useSelector } from 'react-redux';
 import { io } from 'socket.io-client';
 import { getCurrentUserToken } from '@services/authentication/localStorageService';
+import { useParams } from 'react-router-dom';
 import './Terminal.css';
 
-const Terminal = () => {
+const TerminalPage = () => {
+    const { repositoryName } = useParams();
+    const { user } = useSelector(state => state.auth);
     const [socket, setSocket] = useState(null);
+    const [terminalInput, setTerminalInput] = useState('');
+    const [terminalOutput, setTerminalOutput] = useState([
+        <TerminalOutput>Connecting to server...</TerminalOutput>
+    ]);
+
+    const handleTerminalInput = (input) => {
+        setTerminalInput(input);
+        setTerminalOutput([
+            ...terminalOutput,
+            <TerminalOutput>$: {input}</TerminalOutput>
+        ]);
+    };
+
+    const handleCommandResponse = (response) => {
+        setTerminalOutput([
+            ...terminalOutput,
+            <TerminalOutput>{response}</TerminalOutput>
+        ]);
+    };
+
+    useEffect(() => {
+        if(!socket) return;
+        if(terminalInput === 'clear'){
+            setTerminalOutput([]);
+            return;
+        }
+        socket.emit('command', terminalInput, handleCommandResponse);
+    }, [terminalInput]);
 
     useEffect(() => {
         const socket = io(import.meta.env.VITE_SERVER, { 
             transports: ['websocket'],
-            auth: { token: getCurrentUserToken() }
+            auth: { token: getCurrentUserToken() },
+            query: { repositoryName }
         });
         setSocket(socket);
-        return () => socket.close();
+        socket.on('connect', () => {
+            setTerminalOutput([
+                ...terminalOutput,
+                <TerminalOutput>Connection established.</TerminalOutput>
+            ]);
+        });
+        return () => {
+            socket.close();
+        };
     }, []);
 
     return (
         <main id='Repository-Terminal-Main'>
-            <h1>Terminal</h1>
+            <section id='Repository-Terminal-Header-Container'>
+                <article id='Repository-Terminal-Title-Container'>
+                    <h1 id='Repository-Terminal-Title'>Advanced repository management</h1>
+                    <p id='Repository-Terminal-Subtitle'>Interact with the root of your repository through the command line. <br /> A connection with the server will be initiated to manage communication.</p>
+                </article>
+            </section>
+
+            <section id='Repository-Terminal-Body-Container'>
+                <article id='Repository-Terminal'>
+                    <Terminal 
+                        name={repositoryName} 
+                        colorMode={ColorMode.Dark}
+                        onInput={handleTerminalInput}
+                        prompt={`${user.username}@${repositoryName}:~$`}
+                    >
+                        {terminalOutput}
+                    </Terminal>
+                </article>
+            </section>
         </main>
     );
 };
 
-export default Terminal;
+export default TerminalPage;
--- server/controllers/wsController.js
+++ server/controllers/wsController.js
@@ -1,18 +1,40 @@
 const RuntimeError = require('../utilities/runtimeError');
 const { getUserByToken } = require('../middlewares/authentication');
+const Repository = require('../models/repository');
+const { exec } = require('child_process');
+
+const authenticateSocket = async (socket, next) => {
+    const { token } = socket.handshake.auth;
+    if(!token) return next(new RuntimeError('Authentication::Token::Required'));
+    const user = await getUserByToken(token);
+    socket.user = user;
+
+    const { repositoryName } = socket.handshake.query;
+    if(!repositoryName) return next(new RuntimeError('Repository::Name::Required'));
+    const repository = await Repository.findOne({ name: repositoryName, user: user._id });
+    if(!repository) return next(new RuntimeError('Repository::NotFound'));
+    socket.repository = repository;
+
+    next();
+};
+
+const handleCommand = async (command, callback, socket) => {
+    const { repository } = socket;
+    const workingDir = `${__dirname}/../storage/repositories/${repository._id}/`;
+    exec(command, { cwd: workingDir }, (error, stdout, stderr) => {
+        let response = '';
+        if(stdout) response = stdout;
+        if(stderr) response = stderr;
+        if(error) response = error.message;
+        // Sometimes, "stderr" can be a object!
+        if(typeof response === 'object') response = stderr.cmd;
+        callback(response);
+    });
+};
 
 module.exports = (io) => {
-    io.use(async (socket, next) => {
-        const token = socket.handshake.auth.token;
-        if(!token){
-            return next(new RuntimeError('Authentication::Required', 401));
-        }
-        const user = await getUserByToken(token);
-        socket.user = user;
-        next();
-    });
-
+    io.use(authenticateSocket);
     io.on('connection', (socket) => {
-        console.log('connection');
+        socket.on('command', (command, callback) => handleCommand(command, callback, socket));
     });
 };
