118c21c1dcf3454723a4c9ef68e92e9e50083566
rodyherrera <contact@rodyherrera.com>
2024-01-22 02:57:48 +0100

    The "to" attribute can now handle requests indicated in the attribute, both to internal pages of the application and external ones.

--- client/src/components/general/IconLink/IconLink.jsx
+++ client/src/components/general/IconLink/IconLink.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './IconLink.css';
 
-const IconLink = ({ title, icon, ...props }) => {
+const IconLink = ({ title, icon, to = null, ...props }) => {
+    const navigate = useNavigate();
+
+    const handleOnClick = () => {
+        if(props?.onClick) props?.onClick();
+        if(to){
+            if(to.startsWith('http')) window.open(to, '_blank');
+            else navigate(to);
+        }
+    };
 
     return (
-        <div className='Icon-Link-Container' {...props}>
+        <div className='Icon-Link-Container' {...props} onClick={handleOnClick}>
             <span className='Icon-Link-Title'>{title}</span>
             {(icon) && (
                 <i className='Icon-Link-Icon-Container'>
