f325787146b521923ac996b2a25d3052bfece464
rodyherrera <contact@rodyherrera.com>
2024-02-14 15:15:15 +0100

    Breacrumbs && Title + Description && Design adapted to the "File Explorer" page.

--- client/src/pages/protected/repository/Storage/Storage.jsx
+++ client/src/pages/protected/repository/Storage/Storage.jsx
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { formatDate } from '@utilities/runtime';
 import FileExplorer from '@components/repository/FileExplorer';
+import Breadcrumbs from '@components/general/Breadcrumbs';
 import AnimatedMain from '@components/general/AnimatedMain';
 import './Storage.css';
 
@@ -19,6 +20,23 @@ const Storage = () => {
 
     return selectedRepository && (
         <AnimatedMain id='Repository-Storage-Main'>
+            <Breadcrumbs 
+                items={[
+                    { title: 'Home', to: '/' },
+                    { title: 'Dashboard', to: '/dashboard/' },
+                    { title: 'Repositories', to: '/dashboard/' },
+                    { title: selectedRepository.name, to: '/dashboard/' },
+                    { title: 'File Explorer', to: `/repository/${selectedRepository.name}/storage/` }
+                ]}
+            />
+
+            <section id='Repository-Storage-Header-Container'>
+                <article id='Repository-Storage-Header-Title-Container'>
+                    <h1 id='Repository-Storage-Header-Title'>File Explorer</h1>
+                    <p id='Repository-Storage-Header-Description'>You can make changes to the repository files without having to commit within Github, however, when a commit occurs, the files will be overwritten with the repository information in Github, so your changes made locally will be lost.</p>
+                </article>
+            </section>
+
             <div className='File-Explorer-Container'>
                 <div className='File-Explorer-Header-Container'>
                     <div className='File-Explorer-Header-Left-Container'>
