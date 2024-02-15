6566117cd23c26c70dc84d82e4ed6c1b9f2450c7
rodyherrera <contact@rodyherrera.com>
2024-02-15 17:23:25 +0100

    Copyright and license as a comment at the beginning of each file within the server application.

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,3 +1,17 @@
+/***
+ * Copyright (C) Rodolfo Herrera Hernandez. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root
+ * for full license information.
+ *
+ * =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+ *
+ * For related information - https://github.com/rodyherrera/Quantum/
+ *
+ * All your applications, just in one place. 
+ *
+ * =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+****/
+
 const passport = require('passport');
 const GithubStrategy = require('passport-github').Strategy;
 
