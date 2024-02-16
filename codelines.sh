3755ab744dae3165cf058338554e14b1b26dfe8f
rodyherrera <contact@rodyherrera.com>
2024-02-16 03:13:14 +0100

    Meta tags (index.html web-ui).

--- codelines.sh
+++ codelines.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+# * =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+# *
+# * Copyright (C) Rodolfo Herrera Hernandez. All rights reserved.
+# * Licensed under the MIT license. See LICENSE file in the project root
+# * for full license information.
+# *
+# * =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+# *
+# * For related information - https://github.com/rodyherrera/Quantum/
+# *
+# * All your applications, just in one place. 
+# *
+# * =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 echo "[Quantum Cloud]: Counting lines of code..."
 
 cloc --exclude-list=.clocignore .
