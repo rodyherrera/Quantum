774cf594f32d94f8c7095ed5da2012f70cc1595d
rodyherrera <contact@rodyherrera.com>
2024-02-18 05:41:56 +0100

    Refactoring and code splitting of "repositoryHandler" && containerLoggable (for repositories and user).

--- server/utilities/repositoryHandler.js
+++ server/utilities/repositoryHandler.js
@@ -4,62 +4,79 @@ class RepositoryHandler{
     constructor(repository, user){
         this.repository = repository;
         this.user = user;
+        this.workingDir = `/app/github-repos/${repository._id}${repository.rootDirectory}`;
     };
 
     async getOrCreateShell(){
         try{
-            if(global.userContainers[this.user._id]?.[this.repository._id]){
-                return global.userContainers[this.user._id][this.repository._id];
+            const userContainers = global.userContainers[this.user._id];
+            if(userContainers?.[this.repository._id]){
+                return userContainers[this.repository._id];
             }
-            const container = global.userContainers[this.user._id];
-            const exec = await container.instance.exec({
+            const { instance } = userContainers;
+            const exec = await instance.exec({
                 Cmd: ['/bin/ash'],
                 AttachStdout: true,
                 AttachStderr: true,
                 AttachStdin: true,
-                WorkingDir: `/app/github-repos/${this.repository._id}${this.repository.rootDirectory}`,
+                WorkingDir: this.workingDir,
                 Tty: true
             });
             const stream = await exec.start({ hijack: true, stdin: true });
-            global.userContainers[this.user._id][this.repository._id] = stream;
+            userContainers[this.repository._id] = stream;
             return stream;
         }catch(error){
-            console.log('[Quantum Cloud] CRITICAL ERROR (at @utilities/repositoryHandler - createShell):', error);
-            throw error;
+            this.criticalErrorHandler('createShell', error);
         }
     };
 
     async executeInteractiveShell(socket){
         const repositoryShell = await this.getOrCreateShell();
+        this.setupSocketEvents(socket, repositoryShell);
+    };
+
+    setupSocketEvents(socket, repositoryShell){
         socket.emit('history', '...');
         socket.on('command', (command) => repositoryShell.write(command + '\n'));
         repositoryShell.on('data', (chunk) => socket.emit('response', chunk.toString('utf8')));
     };
 
     async start(githubUtility){
-        const {
-            buildCommand,
-            installCommand,
-            startCommand,
-            deployments } = this.repository;
-        const commands = [installCommand, buildCommand, startCommand];
-        if(!buildCommand && !installCommand && !startCommand) return;
-        const currentDeploymentId = deployments[0];
-        const deployment = await Deployment
-            .findById(currentDeploymentId)
-            .select('environment githubDeploymentId status');
+        const commands = this.getValidCommands();
+        if(commands.length === 0) return;
+        const deployment = await this.getCurrentDeployment();
         const formattedEnvironment = deployment.getFormattedEnvironment();
         const repositoryShell = await this.getOrCreateShell();
-        for(const command of commands){
-            if(!command.length) continue;
-            const formattedComamnd = `${formattedEnvironment} ${command}\r\n`;
-            repositoryShell.write(formattedComamnd);
-        }
+        this.executeCommands(commands, formattedEnvironment, repositoryShell);
         const { githubDeploymentId } = deployment;
         githubUtility.updateDeploymentStatus(githubDeploymentId, 'success');
         deployment.status = 'success';
-        deployment.save();
+        await deployment.save();
     };
+
+    getValidCommands(){
+        const { buildCommand, installCommand, startCommand } = this.repository;
+        return [installCommand, buildCommand, startCommand].filter(Boolean);
+    };
+
+    async getCurrentDeployment(){
+        const currentDeploymentId = this.repository.deployments[0];
+        return Deployment
+            .findById(currentDeploymentId)
+            .select('environment githubDeploymentId status');
+    };
+
+    executeCommands(commands, formattedEnvironment, repositoryShell){
+        for(const command of commands){
+            const formattedCommand = `${formattedEnvironment} ${command}\r\n`;
+            repositoryShell.write(formattedCommand);
+        }
+    };
+
+    handleCriticalError(method, error){
+        console.log(`[Quantum Cloud] CRITICAL ERROR (at @utilities/repositoryHandler - ${method}):`, error);
+        throw error;
+    }
 };
 
 module.exports = RepositoryHandler;
