f72ad27dc9ab79cd8923a99521358ffa1b202b74
rodyherrera <contact@rodyherrera.com>
2024-09-13 01:25:18 -0300

    refactor: separation of logic responsible for communication with the server for the interactive console.

--- client/src/components/general/CloudShell/CloudShell.jsx
+++ client/src/components/general/CloudShell/CloudShell.jsx
@@ -1,11 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Terminal } from '@xterm/xterm';
-import { FitAddon } from '@xterm/addon-fit';
-import useWebSocket from '@hooks/useWebSocket';
+import React, { useRef } from 'react';
 import ResizableInAxisY from '@components/general/ResizableInAxisY';
-import './CloudShell.css';
-
-
+import useRemoteTerminal from '@hooks/useRemoteTerminal';
 import { VscGithubAlt } from 'react-icons/vsc';
 import { BiBookAlt } from 'react-icons/bi';
 import { AiOutlineClose } from 'react-icons/ai';
@@ -14,81 +9,12 @@ import { BsTerminal } from 'react-icons/bs';
 import { PiDotsSixBold } from 'react-icons/pi';
 import { useDispatch } from 'react-redux';
 import { setIsCloudConsoleEnabled } from '@services/core/slice';
-
+import './CloudShell.css';
 
 const CloudConsole = () => {
-    const [socket, isConnected] = useWebSocket('Cloud::Console');
-    const termInputValueRef = useRef('');
-    const xtermRef = useRef(null);
     const termContainerRef = useRef(null);
-    const fitAddonRef = useRef(null);
-
-
     const dispatch = useDispatch();
-
-
-    // This function will be used to handle the 'history' event that sends past 
-    // interactions of the user with the terminal (log) and the 'response' event 
-    // that server sends the response to commands that are sent.
-    const onResponse = (data) => {
-        xtermRef.current.write(data);
-    }
-
-    const onKey = ({ domEvent, key }) => {
-        const { keyCode } = domEvent;
-        if(keyCode === 13){
-            socket.emit('command', termInputValueRef.current.trim());
-            termInputValueRef.current = '';
-            xtermRef.current.write('\r\n');
-        }else if(keyCode === 8){
-            // Backspace 
-            if(!termInputValueRef.current.length) return;
-            termInputValueRef.current = termInputValueRef.current.slice(0, -1);
-            xtermRef.current.write('\b \b');
-        }else{
-            // If this block is executed, it is assumed that the 
-            // user is typing in the terminal. Therefore, we write about it.
-            if(key.length === 1){
-                termInputValueRef.current += key;
-                xtermRef.current.write(key);
-            }
-        }
-    };
-
-    const createTerm = () => {
-        xtermRef.current = new Terminal({
-            cursorBlink: true,
-            convertEol: true,
-            fontFamily: 'monospace',
-            fontSize: 14,
-            cols: 128,
-            cursorStyle: 'bar',
-            theme: {
-                foreground: '#ffffff',
-                background: '#000000'
-            }
-        });
-        fitAddonRef.current = new FitAddon();
-        xtermRef.current.loadAddon(fitAddonRef.current);
-        xtermRef.current.open(termContainerRef.current);
-        xtermRef.current.onKey(onKey);
-        fitAddonRef.current.fit();
-    }
-
-    useEffect(() => {
-        if(isConnected){
-            createTerm();
-            socket.on('response', onResponse);
-            socket.on('history', onResponse);
-            return () => {
-                socket.off('response', onResponse);
-                socket.off('history', onResponse);
-                xtermRef.current.dispose();
-            }
-        }
-    }, [isConnected]);
-
-
+    const { isConnected, fitAddonRef } = useRemoteTerminal({ termContainerRef });
 
     const headerIcons = [
         [VscGithubAlt, () => window.open('https://github.com/rodyherrera/Quantum/')],
---
+++ client/src/hooks/useRemoteTerminal.js
@@ -0,0 +1,76 @@
+import useWebSocket from '@hooks/useWebSocket';
+import { Terminal } from '@xterm/xterm';
+import { FitAddon } from '@xterm/addon-fit'
+import { useEffect, useRef } from 'react';
+
+const useRemoteTerminal = ({ termContainerRef }) => {
+    const [socket, isConnected] = useWebSocket('Cloud::Console');
+    const xtermRef = useRef(null);
+    const termInputValueRef = useRef('');
+    const fitAddonRef = useRef(null);
+
+    // This function will be used to handle the 'history' event that sends past 
+    // interactions of the user with the terminal (log) and the 'response' event 
+    // that server sends the response to commands that are sent.
+    const onResponse = (data) => {
+        xtermRef.current.write(data);
+    }
+
+    const onKey = ({ domEvent, key }) => {
+        const { keyCode } = domEvent;
+        if(keyCode === 13){
+            socket.emit('command', termInputValueRef.current.trim());
+            termInputValueRef.current = '';
+            xtermRef.current.write('\r\n');
+        }else if(keyCode === 8){
+            // Backspace 
+            if(!termInputValueRef.current.length) return;
+            termInputValueRef.current = termInputValueRef.current.slice(0, -1);
+            xtermRef.current.write('\b \b');
+        }else{
+            // If this block is executed, it is assumed that the 
+            // user is typing in the terminal. Therefore, we write about it.
+            if(key.length === 1){
+                termInputValueRef.current += key;
+                xtermRef.current.write(key);
+            }
+        }
+    }
+
+    const createTerm = () => {
+        xtermRef.current = new Terminal({
+            cursorBlink: true,
+            convertEol: true,
+            fontFamily: 'monospace',
+            fontSize: 14,
+            cols: 128,
+            cursorStyle: 'bar',
+            theme: {
+                foreground: '#ffffff',
+                background: '#000000'
+            }
+        });
+        fitAddonRef.current = new FitAddon();
+        xtermRef.current.loadAddon(fitAddonRef.current);
+        xtermRef.current.open(termContainerRef.current);
+        xtermRef.current.onKey(onKey);
+        fitAddonRef.current.fit();
+    }
+
+    useEffect(() => {
+        if(isConnected){
+            createTerm();
+            socket.on('response', onResponse);
+            socket.on('history', onResponse);
+            return () => {
+                socket.off('response', onResponse);
+                socket.off('history', onResponse);
+                xtermRef.current.dispose();
+            }
+        }
+    }, [isConnected]);
+
+    return { isConnected, fitAddonRef, xtermRef };
+};
+
+export default useRemoteTerminal;
