3fbea6dd4d47304b09baa14e3becc4274b881c6a
Rodolfo Herrera <contact@rodyherrera.com>
2024-09-16 20:22:10 +0200

    refactor: modularized containerization controller logic.

--- server/controllers/docker/container.ts
+++ server/controllers/docker/container.ts
@@ -6,6 +6,7 @@ import slugify from 'slugify';
 import DockerImage from '@models/docker/image';
 import DockerNetwork from '@models/docker/network';
 import HandlerFactory from '@controllers/common/handlerFactory';
+import { IRequestDockerImage, IRequestDockerNetwork } from '@typings/controllers/docker/container';
 import { isImageAvailable } from '@services/docker/image';
 import { catchAsync } from '@utilities/helpers';
 import { NextFunction, Request, Response } from 'express';
@@ -25,60 +26,78 @@ const DockerContainerFactory = new HandlerFactory({
     ]
 });
 
+const configureContainerStorage = (userId: string, name: string, containerId: string) => {
+    const userContainerPath = path.join('/var/lib/quantum', process.env.NODE_ENV as string, 'containers', userId);
+    const containerStoragePath = path.join(userContainerPath, 'docker-containers', `${slugify(name)}-${containerId}`);
+    return containerStoragePath;
+};
+
+const findOrCreateImage = async (image: string | IRequestDockerImage, userId: string, next: NextFunction) => {
+    let containerImage = null;
+    if(typeof image === 'string'){
+        containerImage = await DockerImage.findById(image).select('_id');
+    }
+    if(!containerImage){
+        const { name, tag } = image as IRequestDockerImage;
+        if(!isImageAvailable(name, tag)){
+            next(new RuntimeError('DockerContainer::CreateDocker::ImageNotFound', 404));
+            return null;
+        }
+        containerImage = await DockerImage.create({ name, tag, user: userId });
+    }
+    return containerImage;
+};
+
+const findOrCreateNetwork = async (network: string | IRequestDockerNetwork, userId: string, next: NextFunction) => {
+    let containerNetwork = null;
+    if(typeof network === 'string'){
+        containerNetwork = await DockerNetwork.findById(network).select('_id');
+    }
+    if(!containerNetwork){
+        const { driver, name } = network as IRequestDockerNetwork;
+        if(!driver || !name){
+            next(new RuntimeError('DockerContainer::CreateDocker::MissingParams', 400));
+            return null;
+        }
+        containerNetwork = await DockerNetwork.create({ user: userId, driver, name });
+    }
+    return containerNetwork;
+};
+
 export const getMyDockerContainers = catchAsync(async (req: Request, res: Response) => {
     const user = req.user as IUser;
     const containers = await DockerContainer.find({ user: user._id });
     res.status(200).json({ status: 'success', data: containers });
 });
 
-// TODO: use handlerFactory for refactor this.
 export const createDockerContainer = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const { image, name, network } = req.body;
     if(!image || !name){
         return next(new RuntimeError('DockerContainer::CreateDocker::MissingParams', 400));
     }
-    
     const user = req.user as IUser;
     const userId = user._id.toString();
-
-    // The image can be an ID of an existing one or in { name, tag } format
-    let containerImage = null;
-    if(typeof image === 'string'){
-        containerImage = await DockerImage.findById(image).select('_id');
-    }
-    if(!containerImage){
-        const { name, tag } = image;
-        const isAvailable = isImageAvailable(name, tag);
-        if(!isAvailable){
-            return next(new RuntimeError('DockerContainer::CreateDocker::ImageNotFound', 404));
-        }
-        containerImage = await DockerImage.create({ name, tag, user: userId });
-    }
-    // If it does not exist, create a new network.
-    let containerNetwork = null;
-    if(typeof containerNetwork === 'string'){
-        containerNetwork = await DockerNetwork.findById(network).select('_id');
-    }
-    if(!containerNetwork){
-        const { driver, name } = network;
-        if(!driver || !name){
-            return next(new RuntimeError('DockerContainer::CreateDocker::MissingParams', 400));
-        }
-        containerNetwork = await DockerNetwork.create({ user: userId, driver, name });
-    }
-    const container = await DockerContainer.create({ name, user: userId, image: containerImage, network: containerNetwork });
-    const containerId = container._id.toString();
     
-    // NOTE: check @services/userContainer.ts 
-    const userContainerPath = path.join('/var/lib/quantum', process.env.NODE_ENV as string, 'containers', userId.toString());
-    const containerStoragePath = path.join(userContainerPath, 'docker-containers', slugify(name) + '-' + containerId);
+    const containerImage = await findOrCreateImage(image, userId, next);
+    if(!containerImage) return;
+    
+    const containerNetwork = await findOrCreateNetwork(network, userId, next);
+    if(!containerNetwork) return;
+
+    const container = await DockerContainer.create({ 
+        name, user: userId, image: containerImage, network: containerNetwork });
+    const containerId = container._id.toString();
+        
+    const containerStoragePath = configureContainerStorage(userId, name, containerId);
     const dockerHandler = new DockerHandler({
         imageName: image.name,
         imageTag: image.tag,
         storagePath: containerStoragePath,
         dockerName: containerId
     });
+
     await DockerContainer.updateOne({ _id: containerId }, { storagePath: containerStoragePath });
     await dockerHandler.createAndStartContainer();
+
     res.status(200).json({ status: 'success', data: dockerHandler });
 });
---
+++ server/typings/controllers/docker/container.d.ts
@@ -0,0 +1,9 @@
+export interface IRequestDockerImage{
+    name: string;
+    tag: string;
+}
+
+export interface IRequestDockerNetwork{
+    name: string;
+    driver: name;
+}
