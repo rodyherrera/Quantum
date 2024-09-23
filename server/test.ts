6b8c4e0ecc6f0acdeddc6b50c892794d08bf4989
Rodolfo Herrera <contact@rodyherrera.com>
2024-09-23 07:55:46 +0200

    remove: test file to obtain docker ip.

--- server/test.ts
+++
@@ -1,9 +0,0 @@
-import Docker from 'dockerode';
-
-(async () => {
-    const docker = new Docker();
-    const container = docker.getContainer('quantum-container-development-HTTP_Server_Test_');
-    const data = await container.inspect();
-    const ipAddress = data.NetworkSettings.Networks['quantum-network-development-66f0f25a392748f51d7a9516-http-server-test-network'].IPAddress;
-    console.log(ipAddress);
-})();
