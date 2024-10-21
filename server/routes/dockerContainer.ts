dd2bb6c98a13bb4bd76620dda81c65dcd082d475
Rodolfo Herrera <contact@rodyherrera.com>
2024-10-21 05:27:06 +0200

    feat(api): endpoint for listing docker container files and ownership middleware.

--- server/routes/dockerContainer.ts
+++ server/routes/dockerContainer.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import * as dockerContainerController from '@controllers/docker/container';
 import * as authMiddleware from '@middlewares/authentication';
+import * as dockerContainerMiddleware from '@middlewares/dockerContainer';
 
 const router = express.Router();
 
 router.get('/random-available-port/', dockerContainerController.randomAvailablePort);
 
 router.use(authMiddleware.protect);
+
+router.get('/storage/:id/explore/:route?',
+    dockerContainerMiddleware.verifyOwnership,
+    dockerContainerController.storageExplorer);
+
 router.get('/me/', dockerContainerController.getMyDockerContainers);
 router.post('/', dockerContainerController.createDockerContainer);
 router.patch('/:id', dockerContainerController.updateDockerContainer);
