deac5b6039adbbfbb185ba5fa87e8409ae218485
rodyherrera <hello@rodyherrera.com>
2024-10-23 20:07:12 -0300

    refactor: optimize Layout component rendering and effect dependencies

--- client/src/components/organisms/Layout/Layout.jsx
+++ client/src/components/organisms/Layout/Layout.jsx
@@ -1,37 +1,21 @@
-/***
- * Copyright (C) Rodolfo Herrera Hernandez. All rights reserved.
- * Licensed under the MIT license. See LICENSE file in the project root
- * for full license information.
- *
- * =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
- *
- * For related information - https://github.com/rodyherrera/Quantum/
- *
- * All your applications, just in one place. 
- *
- * =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-****/
-
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Outlet, useNavigate, useLocation } from 'react-router-dom';
-
 import { authenticateWithCachedToken } from '@services/authentication/utils';
 import { resetErrorForAllSlices } from '@services/core/operations';
-
 import CloudShell from '@components/organisms/CloudShell';
 import Waves from '@components/atoms/Waves';
 import ClickSpark from '@components/atoms/ClickSpark';
 import Banner from '@components/atoms/Banner';
 import Header from '@components/organisms/Header';
 import Menu from '@components/organisms/Menu';
-import useKeyPress from '@hooks/common/useKeyPress'
+import useKeyPress from '@hooks/common/useKeyPress';
 import SquaredBackground from '@components/atoms/SquaredBackground';
-import { useWindowSize } from '@hooks/common/';
+import { useWindowSize } from '@hooks/common';
 import './Layout.css';
 
 const Layout = () => {
-    const { user, authStatus, loadingStatus } = useSelector((state) => state.auth);
+    const { user, authStatus } = useSelector(state => state.auth);
     const { isLoading: githubIsLoading } = useSelector(state => state.github);
     const { isMenuEnabled, isCloudConsoleEnabled } = useSelector(state => state.core);
     const { width } = useWindowSize();
@@ -39,30 +23,28 @@ const Layout = () => {
     const navigate = useNavigate();
     const location = useLocation();
 
-    useKeyPress('Escape', () => {
-        navigate(-1);
-    });
+    useKeyPress('Escape', () => navigate(-1));
 
     useEffect(() => {
         const bodyEl = document.getElementById('QuantumCloud-Body');
-        const overflowValue = isMenuEnabled ? 'hidden' : 'unset';
-        bodyEl.style.overflow = overflowValue;
+        bodyEl.style.overflow = isMenuEnabled ? 'hidden' : 'unset';
     }, [isMenuEnabled]);
 
     useEffect(() => {
         dispatch(resetErrorForAllSlices());
-    }, [location.pathname]);
+    }, [location.pathname, dispatch]);
 
     useEffect(() => {
-        if(authStatus.isAuthenticated) return;
-        authenticateWithCachedToken(dispatch);
-    }, [dispatch, authStatus.isAuthenticated]);
-
+        if(!authStatus.isAuthenticated){
+            authenticateWithCachedToken(dispatch);
+        }
+    }, [dispatch, authStatus]);
+    
     useEffect(() => {
         if(authStatus.isAuthenticated && !user?.github?._id){
             navigate('/github/need-authenticate/');
         }
-    }, [user, loadingStatus.isLoading, authStatus.isCachedAuthLoading, githubIsLoading, authStatus.isAuthenticated]);
+    }, [user?.github?._id, githubIsLoading, authStatus.isAuthenticated, navigate]);
 
     return (
         <React.Fragment>
