f12755f1642cdcfd10ad8d2bbb9c9e584444cedc
rodyherrera <hello@rodyherrera.com>
2024-11-07 00:58:51 -0300

    feat: editing options in dashboard modules.

--- client/src/components/molecules/Docker/DockerImage/DockerImageFooter/DockerImageFooter.jsx
+++ client/src/components/molecules/Docker/DockerImage/DockerImageFooter/DockerImageFooter.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { DashboardCardFooter } from '@components/atoms/DashboardCard';
 import { IoIosRemoveCircleOutline } from 'react-icons/io';
+import { LiaEditSolid } from 'react-icons/lia';
+import { useDispatch } from 'react-redux';
+import { setState as dockerImageSetState } from '@services/docker/image/slice';
 import useDeleteDockerImage from '@hooks/api/docker/useDeleteDockerImage';
 import './DockerImageFooter.css';
 
 const DockerImageFooter = ({ image }) => {
+    const dispatch = useDispatch();
     const deleteDockerImage = useDeleteDockerImage(image._id);
 
+    const selectDockerImage = () => {
+        dispatch(dockerImageSetState({ path: 'selectedDockerImage', value: image }));
+    };
+
     const options = [
-        ['Delete', IoIosRemoveCircleOutline, null, deleteDockerImage]
+        ['Delete', IoIosRemoveCircleOutline, null, deleteDockerImage],
+        ['Edit', LiaEditSolid, `/docker-image/${image._id}/update`, selectDockerImage]
     ];
 
     return <DashboardCardFooter options={options} />
--- client/src/components/molecules/Docker/DockerNetwork/DockerNetworkFooter/DockerNetworkFooter.jsx
+++ client/src/components/molecules/Docker/DockerNetwork/DockerNetworkFooter/DockerNetworkFooter.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { DashboardCardFooter } from '@components/atoms/DashboardCard';
 import { IoIosRemoveCircleOutline } from 'react-icons/io';
+import { useDispatch } from 'react-redux';
+import { setState as dockerNetworkSetState } from '@services/docker/network/slice';
+import { LiaEditSolid } from 'react-icons/lia';
 import useDeleteDockerNetwork from '@hooks/api/docker/useDeleteDockerNetwork';
 import './DockerNetworkFooter.css';
 
 const DockerNetworkFooter = ({ network }) => {
+    const dispatch = useDispatch();
     const deleteDockerNetwork = useDeleteDockerNetwork(network._id);
 
+    const selectDockerNetwork = () => {
+        dispatch(dockerNetworkSetState({ path: 'selectedDockerNetwork', value: network }));
+    };
+
     const options = [
-        ['Delete', IoIosRemoveCircleOutline, null, deleteDockerNetwork]
+        ['Delete', IoIosRemoveCircleOutline, null, deleteDockerNetwork],
+        ['Edit', LiaEditSolid,`/docker-network/${network._id}/update`, selectDockerNetwork]
     ];
 
     return <DashboardCardFooter options={options} />
