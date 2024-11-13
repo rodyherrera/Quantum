702331b8d9947ccf7efd90c98d51a9015cac13ea
rodyherrera <hello@rodyherrera.com>
2024-11-13 03:12:12 -0300

    chore(ui): colors in and borders in events like hover.

--- client/src/components/atoms/DashboardCard/DashboardCardBody/DashboardCardBody.jsx
+++ client/src/components/atoms/DashboardCard/DashboardCardBody/DashboardCardBody.jsx
@@ -4,10 +4,17 @@ import { IoIosMore } from 'react-icons/io';
 import ContextMenu from '@components/organisms/ContextMenu';
 import './DashboardCardBody.css';
 
-const DashboardCardBody = ({ name, headerBadges = null, ctxMenuOpts, updatedAt, createdAt }) => {
+const DashboardCardBody = ({ 
+    name, 
+    headerBadges = null, 
+    ctxMenuOpts,
+    updatedAt, 
+    createdAt, 
+    containerProps 
+}) => {
 
     return (
-        <div className='Dashboard-Card-Body-Container'>
+        <div className='Dashboard-Card-Body-Container' {...containerProps}>
             <div className='Dashboard-Card-Name-Container'>
                 <div className='Dashboard-Card-Header-Left-Container'>
                     <h3 className='Dashboard-Card-Name'>{name}</h3>
--- client/src/components/molecules/Docker/DockerContainer/DockerContainerHeader/DockerContainerHeader.jsx
+++ client/src/components/molecules/Docker/DockerContainer/DockerContainerHeader/DockerContainerHeader.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { humanFileSize } from '@utilities/common/fileUtils';
 import { DashboardCardHeader } from '@components/atoms/DashboardCard';
 import './DockerContainerHeader.css';
 
 const DockerContainerHeader = ({ container }) => {
+    useEffect(() => {
+        console.log(container.name, container)
+    }, []);
     const options = [
         container.ipAddress ? `IPv4: ${container.ipAddress}` : 'Unallocated Subnet IP',
         `${container.image.name}:${container.image.tag} ${humanFileSize(container.image.size)}`,
