898dc833cf30f298e0066d6d77a2361682387261
rodyherrera <hello@rodyherrera.com>
2024-12-05 01:57:45 -0300

    feat(components): added <Input /> component and form with basic fields in Setup Utility Page.

---
+++ setup-utility/client/src/components/atoms/Input/Input.tsx
@@ -0,0 +1,21 @@
+import './Input.css';
+
+interface InputProps {
+    helperText?: string;
+}
+
+const Input: React.FC<InputProps> = ({ helperText, ...props }) => {
+    return (
+        <div className='Input-Container'>
+            <div className='Input'>
+                <input {...props} />
+            </div>
+
+            {helperText && (
+                <p className='Input-Helper-Text'>{helperText}.</p>
+            )}
+        </div>
+    );
+};
+
+export default Input;
