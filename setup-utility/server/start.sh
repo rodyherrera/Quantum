b8238df0192be70ada8494482425f82842473ea6
rodyherrera <hello@rodyherrera.com>
2024-12-06 01:08:18 -0300

    feat(setup-utility): installation of dependencies if they are not installed and success message when running server.

--- setup-utility/server/start.sh
+++ setup-utility/server/start.sh
@@ -4,16 +4,31 @@
 CORES=$(nproc)
 WORKERS=$(( 2 * CORES + 1 ))
 
-pip install -r requirements.txt
+# Define host and port variables
+HOST="0.0.0.0"
+PORT="8000"
+
+# Check if requirements.txt has already been installed
+if ! pip check > /dev/null 2>&1; then
+    echo "@setup-utility: installing requirements..."
+    pip install -r requirements.txt
+else
+    echo "@setup-utility: requirements already installed."
+fi
 
 # Production settings
 uvicorn main:app \
-    --host 0.0.0.0 \
-    --port 8000 \
+    --host $HOST \
+    --port $PORT \
     --workers $WORKERS \
     --log-level critical \
     --proxy-headers \
     --forwarded-allow-ips '*' \
     --timeout-keep-alive 65 \
     --access-log \
-    --no-server-header
+    --no-server-header &
+
+echo "@setup-utility: server deployed successfully and running on http://$HOST:$PORT"
+
+# Wait for uvicorn to finish
+wait
