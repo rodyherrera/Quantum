d6376ef5a268cb7355f3ec0ecaff6b0cb73f9785
rodyherrera <hello@rodyherrera.com>
2024-12-06 01:07:45 -0300

    feat(setup-utility): controllers for reading and writing environment variables.

--- setup-utility/server/main.py
+++ setup-utility/server/main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv, dotenv_values
+from pathlib import Path
 import requests
+import os
 
 app = FastAPI()
+env_path = Path(__file__).resolve().parent.parent.parent / '.env'
+load_dotenv(dotenv_path=env_path)
 
-# Configure CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
@@ -12,6 +16,25 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
+@app.get('/env')
+async def get_env():
+    env_vars = dotenv_values(env_path)
+    return env_vars
+
+@app.post('/env')
+async def set_env(request: Request):
+    try:
+        new_vars = await request.json()
+        with open(env_path, 'w') as env_file:
+            for key, value in new_vars.items():
+                env_file.write(f'{key} = {value}\n')
+                os.environ[key] = value
+        return {
+            'status': 'success'
+        }
+    except Exception as e:
+        raise HTTPException(status_code = 500, detail=str(e))
 
 @app.get('/host-ip') 
 async def get_host_ip():
