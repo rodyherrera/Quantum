556fa6431e0e5192e61b5c9d60b7864c01369e11
rodyherrera <hello@rodyherrera.com>
2024-12-06 03:25:51 -0300

    feat(setup-utility): Quantum Docker image build and deployment.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 DEBIAN_FRONTEND=noninteractive
 
 bash install_docker.sh
+
+echo '@deploy.sh: Building and starting the Docker containers...'
+echo '@deploy.sh: This may take a few minutes.'
+
+docker compose up -d --build 
