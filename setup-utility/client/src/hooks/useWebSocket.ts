3bfef885bd576b33c24ce3409542ca1f979dadde
rodyherrera <hello@rodyherrera.com>
2024-12-06 16:25:27 -0300

    chore(hooks): try to reconnect with the server in case you lose connection and notification if it takes too long.

--- setup-utility/client/src/hooks/useWebSocket.ts
+++ setup-utility/client/src/hooks/useWebSocket.ts
@@ -1,30 +1,68 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { addToast } from '@services/toast/slice';
+
+const CHECK_INTERVAL = 5000;
+const RECONNECT_DELAY = 1000;
 
 const useWebSocket = () => {
     const [messages, setMessages] = useState<string[]>([]);
     const [isConnected, setIsConnected] = useState(false);
     const socketRef = useRef<WebSocket | null>(null);
+    const dispatch = useDispatch();
 
-    useEffect(() => {
-        socketRef.current = new WebSocket(import.meta.env.VITE_SERVER + '/ws');
-        socketRef.current.onopen = () => {
+    const connect = useCallback(() => {
+        const socket = new WebSocket(`${import.meta.env.VITE_SERVER}/ws`);
+        socketRef.current = socket;
+
+        socket.onopen = () => {
             setIsConnected(true);
         };
-        socketRef.current.onmessage = (event) => {
+
+        socket.onmessage = (event) => {
             setMessages((prev) => [...prev, event.data]);
         };
-        socketRef.current.onclose = () => {
+
+        socket.onclose = () => {
             setIsConnected(false);
+            setTimeout(() => {
+                connect();
+            }, RECONNECT_DELAY);
         };
-        socketRef.current.onerror = (error) => {
-            console.error(error);
+
+        socket.onerror = (error) => {
+            console.error('WebSocket Error:', error);
+            socket.close();
         };
+    }, []);
+
+    useEffect(() => {
+        connect();
         return () => {
             if(socketRef.current){
                 socketRef.current.close();
             }
-        }
-    }, []);
+        };
+    }, [connect]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if(!isConnected){
+                dispatch(addToast({
+                    id: `connection-timeout-${Date.now()}`,
+                    message: 'The connection to the server has not yet been established.',
+                    type: 'warning',
+                    duration: 5000
+                }));
+            }else{
+                clearInterval(interval);
+            }
+        }, CHECK_INTERVAL);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [isConnected, dispatch]);
 
     const sendMessage = (message: string) => {
         if(socketRef.current && socketRef.current.readyState === WebSocket.OPEN){
