a140d1e8480a818ea956c75a89c1b90f99931759
Rodolfo Herrera <contact@rodyherrera.com>
2024-12-15 09:07:04 +0100

    fix(useKeyPress): handle undefined keyCombo and e.key to prevent toLowerCase errors.

--- client/src/hooks/common/useKeyPress.js
+++ client/src/hooks/common/useKeyPress.js
@@ -1,23 +1,23 @@
 import { useEffect } from 'react';
 
 const useKeyPress = (keyCombo, callback) => {
-    const keyPressHandler = (e) => {
-        const keys = {
-            Control: e.ctrlKey,
-            Shift: e.shiftKey,
-            Alt: e.altKey,
-            // MacOS command key
-            Meta: e.metaKey
-        }
-        const pressedKey = e.key.toLowerCase();
-        const combo = keyCombo.toLowerCase().split('+');
-        const isValidCombo = combo.every((key) => {
-            return keys[combo] || key === pressedKey;
-        });
-        if(isValidCombo) callback(e);
-    };
-
     useEffect(() => {
+        const keyPressHandler = (e) => {
+            if(!e.key) return;
+            const keys = {
+                control: e.ctrlKey,
+                shift: e.shiftKey,
+                alt: e.altKey,
+                meta: e.metaKey
+            };
+            const pressedKey = e.key.toLowerCase();
+            const combo = keyCombo.toLowerCase().split('+');
+            const isValidCombo = combo.every((key) => {
+                return keys[key] || key === pressedKey;
+            });
+            if(isValidCombo) callback(e);
+        };
+
         window.addEventListener('keydown', keyPressHandler);
         return () => {
             window.removeEventListener('keydown', keyPressHandler);
