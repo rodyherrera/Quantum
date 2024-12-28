ba06a51490a835394d58f7af0b2414654fc30199
Rodolfo Herrera <contact@rodyherrera.com>
2024-12-28 21:41:36 +0100

    feat: enhace Docker image handling for better efficiency and robustness

--- server/services/docker/image.ts
+++ server/services/docker/image.ts
@@ -5,14 +5,10 @@ const docker = new Dockerode();
 
 export const isImageAvailable = async (imageName: string, tag: string = 'latest'): Promise<boolean> => {
     const fullImageName = `${imageName}:${tag}`;
-    const image = docker.getImage(fullImageName);
     try{
-        await image.inspect();
-        return true;
+        const images = await docker.listImages({ filters: fullImageName });
+        return images.some((image) => image.RepoTags?.includes(fullImageName));
     }catch(error: any){
-        if(error.statusCode === 404){
-            return false;
-        }
         logger.error('@services/docker/image.ts (isImageAvailable): ' + error);
         throw error;
     }
@@ -20,33 +16,30 @@ export const isImageAvailable = async (imageName: string, tag: string = 'latest'
 
 export const getImageSize = async (imageName: string, tag: string) => {
     const fullImageName = `${imageName}:${tag}`;
-    const image = docker.getImage(fullImageName);
-    const details = await image.inspect();
-    return details.Size;
+    try{
+        const image = docker.getImage(fullImageName);
+        const details = await image.inspect();
+        return details.Size;
+    }catch(error: any){
+        logger.error(`@services/docker/image.ts (getImageSize): ${error.message}`);
+        throw error;
+    }
 }
 
 export const pullImage = async (imageName: string, tag: string = 'latest'): Promise<void> => {
+    const fullImageName = `${imageName}:${tag}`;
     try{
-        const fullImageName = `${imageName}:${tag}`;
         const isAvailable = await isImageAvailable(imageName, tag);
         if(isAvailable) return;
 
         logger.info(`@services/docker/image.ts (pullImage): Pulling "${fullImageName}"...`);
+        const stream = await docker.pull(fullImageName);
         await new Promise<void>((resolve, reject) => {
-            docker.pull(fullImageName, (error: any, stream: NodeJS.ReadableStream) => {
-                if(error){
-                    reject(error);
-                    return;
-                }
-                docker.modem.followProgress(stream, (progressError) => {
-                    if(progressError) reject(progressError);
-                    else resolve();
-                });
-            });
+            docker.modem.followProgress(stream, (err: any) => (err ? reject(err) : resolve()));
         });
-
         logger.info(`@services/docker/image.ts (pullImage): Image "${fullImageName}" downloaded.`);
-    }catch(error){
+    }catch(error: any){
         logger.error('@services/docker/image.ts (pullImage): ' + error);
+        throw error;
     }
 }
