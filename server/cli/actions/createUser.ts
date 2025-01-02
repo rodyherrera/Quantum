d36b8e17e2fe8c8fe93ecc43394df5fe3cd24555
Rodolfo Herrera <contact@rodyherrera.com>
2025-01-02 22:34:52 +0100

    chore(cli): improvement in prompt messages to create the user.

--- server/cli/actions/createUser.ts
+++ server/cli/actions/createUser.ts
@@ -1,27 +1,37 @@
-/***
- * Copyright (C) Rodolfo Herrera Hernandez. All rights reserved.
- * Licensed under the MIT license. See LICENSE file in the project root
- * for full license information.
- *
- * =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
- *
- * For related information - https://github.com/rodyherrera/Quantum/
- *
- * All your applications, just in one place. 
- *
- * =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-****/
-
 import prompts from 'prompts';
 import User from '@models/user';
+import validator from 'validator';
 
 const createUser = async (): Promise<any> => {
     const data = await prompts([
-        { type: 'text', name: 'username', message: 'Username' },
-        { type: 'text', name: 'fullname', message: 'Your fullname' },
-        { type: 'text', name: 'email', message: 'Email address' },
-        { type: 'password', name: 'password', message: 'Password' },
-        { type: 'password', name: 'passwordConfirm', message: 'Confirm password' },
+        { 
+            type: 'text', 
+            name: 'username', 
+            message: 'Username (8-16 characters)', 
+            validate: value => value.length >= 8 && value.length <= 16 ? true : 'Username must be between 8 and 16 characters'
+        },
+        { 
+            type: 'text', 
+            name: 'fullname', 
+            message: 'Your fullname (8-32 characters)', 
+            validate: value => value.length >= 8 && value.length <= 32 ? true : 'Fullname must be between 8 and 32 characters'
+        },
+        { 
+            type: 'text', 
+            name: 'email', 
+            message: 'Email address', 
+            validate: value => validator.isEmail(value) ? true : 'Invalid email address'
+        },
+        { 
+            type: 'password', 
+            name: 'password', 
+            message: 'Password (8-16 characters)'
+        },
+        { 
+            type: 'password', 
+            name: 'passwordConfirm', 
+            message: 'Confirm password (must match password)'
+        },
         {
             type: 'select',
             name: 'role',
@@ -32,8 +42,10 @@ const createUser = async (): Promise<any> => {
             message: 'Role'
         }
     ]);
+
     const newUser = await User.create(data);
-    console.log('[Quantum Manager]: User created ->', newUser);
+    console.log('Quantum Manager: You have successfully created the user. Now through your username and password you can log in to the application');
+    console.log('Quantum Manager: Happy Hacking', newUser.fullname, ' :)');
 };
 
 export default createUser;
