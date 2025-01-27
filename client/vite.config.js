a93a5f4581ce4bff71081aec2a69996db762b52c
Rodolfo Herrera <contact@rodyherrera.com>
2025-01-27 20:09:30 +0100

    fix(vite.config.js): new vite update "allowedHost" added

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,15 +13,14 @@
 ****/
 
 import { defineConfig } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-   plugins: [/*basicSsl()*/, react()],
+   plugins: [react()],
    server: {
       host: '0.0.0.0',
       port: 5000,
-      //https: true
+      allowedHosts: true
    },
    resolve: {
       alias: {
