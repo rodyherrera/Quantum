14fe9cebff4b695bc78976655b45190b587331b4
Rodolfo Herrera <contact@rodyherrera.com>
2025-01-31 22:25:33 +0100

    fix(repository): access to file system through "Build & Dev Settings" in repository

--- client/src/components/molecules/RelatedRepositorySections.jsx
+++ client/src/components/molecules/RelatedRepositorySections.jsx
@@ -46,7 +46,7 @@ const RelatedRepositorySections = ({ isRepositorySelected = true }) => {
             title: 'File Explorer', 
             description: 'Explore and manage your files and folders directly from the browser.',
             icon: CiServer,
-            to: `/repository/${encodeURIComponent(selectedRepository?.alias)}/storage/`
+            to: `/docker-container/${encodeURIComponent(selectedRepository?.container)}/storage/`
         },
         { 
             title: 'Repository CLI', 
